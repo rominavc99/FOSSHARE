58df9f5862d75483e6e0f1848c16794b718cd6e9
rominavc99 <romina.v@iw.digital>
2025-08-11 21:36:36 -0700

    update settings

--- fosshare-backend/config/settings.py
+++ fosshare-backend/config/settings.py
@@ -124,6 +124,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'sslmode': 'require'
         },
     },
 ]
